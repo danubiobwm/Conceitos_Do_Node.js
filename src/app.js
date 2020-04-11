8ef17ec45d4f52e0fd9b0729bae747ef833b0d2e
danubiobwm <danubio.bwm@gmail.com>
2020-04-11 20:52:10 -0300

    Desafio 1 concluído

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, like: 0 };
+
+  repositories.push(repository);
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  if (request.body.like) {
+    return response.status(404).json(repositories[repositoryIndex]);
+  }
+  const repository = {
+    id,
+    url: request.body.url,
+    title: request.body.title,
+    techs: request.body.techs,
+  };
+  repositories[repositoryIndex] = repository;
+  return response.json(repositories[repositoryIndex]);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repositoy not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id}=request.params
+  const repositoryIndex=repositories.findIndex(
+    (repository)=>repository.id===id
+  );
+  if(repositoryIndex<0){
+    return response.status(400).json({error:"Repository not found"});
+  }
+  repositories[repositoryIndex].like +=1;
+  return response.json(repositories[repositoryIndex])
+});
+
+module.exports = app;
